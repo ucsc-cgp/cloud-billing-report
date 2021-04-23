6e93ad8126a83694f5f0cd53c0cee7769bf839d5
richard-hansen <richie.hansen@gmail.com>
2021-04-23 12:58:42 -0400

    tuning for the report example

--- scripts/run-report.sh
+++ scripts/run-report.sh
@@ -7,21 +7,21 @@ REPORT_TYPE=$1
 FAIL_LOG=/root/reporting/fail.log
 AWS_PROFILE="fill me in"
 EMAIL_TMP_FILE=/tmp/${REPORT_TYPE}.eml
-PERSONALIZED_EMAIL_DIR=/tmp/personalizedEmails/
+PERSONALIZED_EMAIL_DIR=/tmp/personalizedEmails
 
 echo "Running container"
 
 # Mount the config file, aws credentials, and a tmp directory into the docker container.
 # The tmp directory will get populated with personalized emails.
-(/usr/bin/docker pull ${IMAGE} > /dev/null 2>&1 && \
-  /usr/bin/docker run \
+(/usr/local/bin/docker run \
   -v ${CONFIG}:/config.json:ro \
   -v ~/.aws/credentials:/root/.aws/credentials:ro \
   -e AWS_PROFILE=${AWS_PROFILE} \
-  -v ${PERSONALIZED_EMAIL_DIR}:/tmp/personalizedEmails \
-  ${IMAGE} ${REPORT_TYPE} > ${EMAIL_TMP_FILE}) || echo "${REPORT_TYPE},$(date -d 'today - 1day' +%Y-%m-%d)" >> ${FAIL_LOG}
+  -v ${PERSONALIZED_EMAIL_DIR}/:/tmp/personalizedEmails \
+  ${IMAGE} ${REPORT_TYPE} > ${EMAIL_TMP_FILE} && \
+  /usr/sbin/sendmail -t < ${EMAIL_TMP_FILE}) || echo "${REPORT_TYPE},$(date -d 'today - 1day' +%Y-%m-%d)" >> ${FAIL_LOG}
 
-for filename in ${PERSONALIZED_EMAIL_DIR}*.eml; do
+for filename in ${PERSONALIZED_EMAIL_DIR}/*.eml; do
     # send the email file
     /usr/sbin/sendmail -t < $filename
     # remove the file
