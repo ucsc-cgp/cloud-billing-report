6ba908dd2f7808b59e60d5b238c2fbd9cde488bf
richard-hansen <richie.hansen@gmail.com>
2021-04-23 12:51:41 -0400

    minor script changes

--- scripts/run-report.sh
+++ scripts/run-report.sh
@@ -5,10 +5,9 @@ CONFIG=/root/reporting/config.json
 IMAGE=ghcr.io/ucsc-cgp/cloud-billing-report:latest
 REPORT_TYPE=$1
 FAIL_LOG=/root/reporting/fail.log
-GOOGLE_ACCOUNT_CREDENTIALS=/root/reporting/gcp-credentials.json
 AWS_PROFILE="fill me in"
 EMAIL_TMP_FILE=/tmp/${REPORT_TYPE}.eml
-PERSONALIZED_EMAIL_DIR = /tmp/personalizedEmails/
+PERSONALIZED_EMAIL_DIR=/tmp/personalizedEmails/
 
 echo "Running container"
 
@@ -20,7 +19,7 @@ echo "Running container"
   -v ~/.aws/credentials:/root/.aws/credentials:ro \
   -e AWS_PROFILE=${AWS_PROFILE} \
   -v ${PERSONALIZED_EMAIL_DIR}:/tmp/personalizedEmails \
-  ${IMAGE} ${REPORT_TYPE} > ${EMAIL_TMP_FILE}) || echo "aws,$(date -d 'today - 1day' +%Y-%m-%d)" >> ${FAIL_LOG}
+  ${IMAGE} ${REPORT_TYPE} > ${EMAIL_TMP_FILE}) || echo "${REPORT_TYPE},$(date -d 'today - 1day' +%Y-%m-%d)" >> ${FAIL_LOG}
 
 for filename in ${PERSONALIZED_EMAIL_DIR}*.eml; do
     # send the email file
