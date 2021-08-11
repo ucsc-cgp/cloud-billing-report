d5565a018fdc0de6e70cae0107fe6ffd3538fd24
richard-hansen <richie.hansen@gmail.com>
2021-08-11 14:51:02 -0400

    estimate GB usage

--- report.py
+++ report.py
@@ -234,8 +234,11 @@ class AWSReport(Report):
         service_by_account_today = nested_dict()
         summary_by_service = nested_dict()
         total_service_cost = collections.defaultdict(Decimal)
+        total_s3_storage = collections.defaultdict(float)
         resource_by_id = {}
+
         today = self.date.strftime('%Y-%m-%d')
+        days = datetime.now().day
 
         # Create a list of resource objects based on their compliance status
         compliance_list = self.generate_compliance_list()
@@ -285,6 +288,29 @@ class AWSReport(Report):
                 summary_by_service[service][usage_type] += amount
             total_service_cost[service] += amount
 
+            # The description contains the billing
+            description = row['lineItem/LineItemDescription']
+
+            # This is pretty gross, but AWS doesn't have a single standardized 'you were charged for X GB' column,
+            # so we need to parse out the rows that are charges for timed storage, where we are charged per byte per hour
+            # and the billing unit is in the form of GB.
+            if service == "Amazon Simple Storage Service" and \
+                    "timedstorage" in usage_type.lower() and \
+                    'bytehrs' in usage_type.lower() and \
+                    'per GB' in description and \
+                    description[0] == '$':
+
+                # remove any region prefix
+                usage_type_partition = usage_type.partition("TimedStorage")
+                new_usage_type = usage_type_partition[1] + usage_type_partition[2]
+
+                # dollars per GB per hour
+                dollars_per_GB = float(description[1:description.find(' ')])
+                GB_per_dollar = 1 / dollars_per_GB
+
+                # We are charged every 1 hour for each byte we have, at a rate of GB / dollar
+                total_s3_storage[new_usage_type] += float(amount) / float(row['lineItem/BlendedRate'])
+
         top_resources = dict(sorted(resource_by_id.items(), key=lambda x: x[1].monthly_cost, reverse=True)[:30])
         total_service_cost = dict(sorted(total_service_cost.items(), key=lambda x: x[1], reverse=True)[:min(len(total_service_cost.items()), 20)])
         for service in summary_by_service:
@@ -298,7 +324,9 @@ class AWSReport(Report):
             noncompliant_resource_by_account=noncompliant_resource_by_account,
             top_resources=top_resources,
             summary_by_service=summary_by_service,
-            total_service_cost=total_service_cost
+            total_service_cost=total_service_cost,
+            total_s3_storage=total_s3_storage,
+            days=days
         )
 
 
